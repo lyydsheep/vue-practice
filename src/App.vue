2b6bd6836a03b7f9a3c5fdafc8b6904a61af0ecf
lyydsheep <2230561977@qq.com>
2025-07-25 14:56:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h1>你好呀！</h1>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.app {
+  text-align: center;
+  margin-top: 50px;
+  font-size: 20px;
+  color: red;
+  font-weight: bold;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
