80b52f92b642edafeb85f79b152f56719ef3754e
lyydsheep <2230561977@qq.com>
2025-07-25 15:41:58 +0800

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
-  <div class="app">
-    <h1>你好呀！</h1>
-  </div>
-
+  <Person/>
 </template>
 
 <script setup lang="ts">
-
+import Person from "./components/Person.vue"
 </script>
 
 <style scoped>
-.app {
-  text-align: center;
-  margin-top: 50px;
-  font-size: 20px;
-  color: red;
-  font-weight: bold;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-}
+
 </style>
---
+++ src/components/Person.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const name = ref('张三')
+const age = ref(18)
+const tel = ref("12306")
+
+function queryTel() {
+  alert(tel.value)
+}
+
+function changeName() {
+  name.value = "zhang-san"
+}
+
+function changeAge() {
+  age.value = 19
+}
+
+</script>
+
+<template>
+  <div class="person">
+    <h1>姓名：{{ name }}</h1>
+    <h1>年龄：{{ age }}</h1>
+    <button @click="queryTel">查询电话号码</button>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+  </div>
+</template>
+
+<style scoped>
+.person {
+  width: 100%;
+  background: aqua;
+}
+
+button {
+  margin: 0 10px;
+}
+</style>
